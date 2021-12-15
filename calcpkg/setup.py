fb8b12f0f7ffc4b3a5e4bffccf9610b29d5e9201
hellokeyworld <ejyoung90@yuhs.ac>
2021-12-15 21:51:06 +0900

    mofiy

--- calcpkg/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='MOTIE using Neopepsee',
-version='3.0.0.2',
-description='Test Package',
-author='TGIL, YONSEI UNIVERSITY',
-url='https://github.com/hellokeyworld/MOTIE',
-python_requires='>=2.7.15',
-)
